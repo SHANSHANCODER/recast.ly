0b39665983787c6f19b41f31fcbe9646106f87b6
Jerremy Stroman <jerremystroman@gmail.com>
2022-03-08 14:24:19 -0800

    Search Youtube helper function

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -4,8 +4,24 @@ $.ajaxPrefilter(function (settings, _, jqXHR) {
   jqXHR.setRequestHeader('Authorization', API_KEY);
 });
 
+// Another helper function?
+
 var searchYouTube = (query, callback) => {
   // TODO
+  let endpoint = 'https://app-hrsei-api.herokuapp.com/api/recastly/videos';
+  $.ajax({
+    url: endpoint,
+    method: 'GET',
+    data: {
+      q: query,
+    },
+    contentType: 'application/json',
+    success: (response) => {
+      console.log(response);
+      callback(response);
+    },
+    error: (err)=>{ console.log('error with ajax GET thing', err); }
+  });
 };
 
 export default searchYouTube;
